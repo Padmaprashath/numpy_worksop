4c1cb60dfe07462cea50639ae3c4bc0d7fcc6dcd
Padmaprashath <padmaprashath.s2023ai-ds@sece.ac.in>
2024-05-20 14:58:31 +0530

    answer

--- question4.py
+++ question4.py
@@ -1,1 +1,8 @@
 #write a program to find the sum of digits of a given number'
+a=int(input("Enter a digit:"))
+c=0
+while a!=0:
+    b=a%10
+    c=c+b
+    a//=10
+print("The sum of the given digits:",c)
--- question6.py
+++ question6.py
@@ -1,1 +1,10 @@
 #write a program to find the maximum of two numbers
+def max_of_two(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+num1=int(input("Enter a number:"))
+num2=int(input("Enter another number:"))
+maximum = max_of_two(num1, num2)
+print(f"The maximum of {num1} and {num2} is {maximum}.")
