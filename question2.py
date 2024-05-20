a70eecc82f6002d8699c5f178f469427c1543952
Padmaprashath <padmaprashath.s2023ai-ds@sece.ac.in>
2024-05-20 14:46:14 +0530

    answer

--- question2.py
+++ question2.py
@@ -1,1 +1,11 @@
 # find if the given number is a palindrome or not?
+a=int(input("Enter a number:"))
+c=0
+while a!=0:
+    b=a%10
+    c=c*10+b
+    a//=10
+if a==c:
+    print("It is a plaindrome")
+else:
+    print("It is not a palindrome")
--- question3.py
+++ question3.py
@@ -1,1 +1,9 @@
 #write a program to find the factorial of a nummber
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*fact(n-1)
+n=int(input("Enter a number:"))
+a=fact(n)
+print("The factorial of the given number:",a)
